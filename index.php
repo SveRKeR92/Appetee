eb271b20bd698526f91c498341b07ad5791dac77
Maxime <maxime.attala@edu.devinci.fr>
2021-01-17 12:48:40 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Appetee, une application créée par des étudiants pour cuisiner en groupe et que tout le monde puisse finir son assiette !">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Appetee</title>
+</head>
+<body>
+    <header>
+        <div>
+            <a href="#"><img src="img/logo.png" alt="logo Appetee"></a>
+        </div>
+        <nav>
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
